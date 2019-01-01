b9622b8f84e42d6778c05e9293bcb2032c40d823
Ray <xiaosumay@gmail.com>
2019-01-01 15:40:13 +0800

    format

--- functions/funny.sh
+++ functions/funny.sh
@@ -1,5 +1,5 @@
 function list_of_commands_you_use_most_often() {
-    history | awk '{a[$2]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head
+    history | awk '{a[$2]++}END{for(i in a){printf "%4s %s\n" , a[i], i}}' | sort -rn | head
 }
 
 function watch_star_wars_via_telnet() {
